6f90f24b00782bc3116c171b297d6cef04218e99
michaelbulaongdev <michaelbulaongdev@gmail.com>
2023-03-20 11:12:25 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,69 +1,70 @@
-import "./App.css";
-import { useEffect, useState } from "react";
+import './App.css';
+import {useEffect, useState} from 'react';
 
-const MousePosition = ({ render }) => {
-  const [mousePosition, setMousePosition] = useState({
-    x: 0,
-    y: 0,
-  });
+const MousePosition = ({render}) => {
+	const [mousePosition, setMousePosition] = useState({
+		x: 0,
+		y: 0,
+	});
 
-  useEffect(() => {
-    const handleMousePositionChange = (e) => {
-      setMousePosition({
-        x: e.clientX,
-        y: e.clientY,
-      })
-    };
+	useEffect(() => {
+		const handleMousePositionChange = (e) => {
+			setMousePosition({
+				x: e.clientX,
+				y: e.clientY,
+			});
+		};
 
-    window.addEventListener("mousemove", handleMousePositionChange);
+		window.addEventListener('mousemove', handleMousePositionChange);
 
-    return () => {
-      window.removeEventListener("mousemove", handleMousePositionChange);
-    };
-  }, []);
+		return () => {
+			window.removeEventListener('mousemove', handleMousePositionChange);
+		};
+	}, []);
 
-  return render(mousePosition);
+	return render(mousePosition);
 };
 
 const PanelMouseLogger = () => {
-  return (
-    <div className="BasicTracker">
-      <p>Mouse position:</p>
-        <MousePosition
-          render={(mousePosition) => {
-            return (
-              <div className="Row">
-                <span>x: {mousePosition.x}</span>
-                <span>y: {mousePosition.y}</span>
-              </div>
-            )
-        }}
-      />
-    </div>
-  );
+	return (
+		<div className='BasicTracker'>
+			<p>Mouse position:</p>
+			<MousePosition
+				render={(mousePosition) => {
+					return (
+						<div className='Row'>
+							<span>x: {mousePosition.x}</span>
+							<span>y: {mousePosition.y}</span>
+						</div>
+					);
+				}}
+			/>
+		</div>
+	);
 };
 
 const PointMouseLogger = () => {
-  return (
-    <MousePosition
-      render={(mousePosition) => {
-        return (
-          <p>
-            ({mousePosition.x}, {mousePosition.y})
-          </p>)
-      }}
-    />
-  )
+	return (
+		<MousePosition
+			render={(mousePosition) => {
+				return (
+					<p>
+						({mousePosition.x}, {mousePosition.y})
+					</p>
+				);
+			}}
+		/>
+	);
 };
 
 function App() {
-  return (
-    <div className="App">
-      <header className="Header">Little Lemon Restaurant 🍕</header>
-      <PanelMouseLogger />
-      <PointMouseLogger />
-    </div>
-  );
+	return (
+		<div className='App'>
+			<header className='Header'>Little Lemon Restaurant 🍕</header>
+			<PanelMouseLogger />
+			<PointMouseLogger />
+		</div>
+	);
 }
 
 export default App;
